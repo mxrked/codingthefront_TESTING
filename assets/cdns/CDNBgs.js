388ceff59a1a9487e3d946fe8a2e8c805e646ebd
Parker <pp101socials@gmail.com>
2023-05-22 12:47:27 -0400

    Created file

---
+++ assets/cdns/CDNBgs.js
@@ -0,0 +1,16 @@
+/**
+ *
+ *  This holds the urls/srcs for the bgs
+ *
+ */
+
+import { CDNBGReturn } from "./CDNReturns";
+
+const BG_INDEX_SCRATCHED_RECOLORED = CDNBGReturn(
+  "index",
+  "scratched-recolored",
+  "webp"
+);
+const BG_INDEX_TOP = CDNBGReturn("index", "top", "webp");
+
+export { BG_INDEX_SCRATCHED_RECOLORED, BG_INDEX_TOP };
---
+++ assets/cdns/CDNImgs.js
@@ -0,0 +1,29 @@
+/**
+ *
+ *  This holds the urls/srcs for the imgs
+ *
+ */
+
+import { CDNImgReturn } from "./CDNReturns";
+
+const IMG_LOTTIE_CONTACT_WHEEL = CDNImgReturn(
+  "lottie",
+  "contact-wheel",
+  "json"
+);
+const IMG_LOTTIE_CSS = CDNImgReturn("lottie", "css", "json");
+const IMG_LOTTIE_HTML = CDNImgReturn("lottie", "html", "json");
+const IMG_LOTTIE_REACT_CIRCLE = CDNImgReturn("lottie", "react-circle", "json");
+const IMG_LOTTIE_WEBSITE_BUILDING = CDNImgReturn(
+  "lottie",
+  "website-building",
+  "json"
+);
+
+export {
+  IMG_LOTTIE_CONTACT_WHEEL,
+  IMG_LOTTIE_CSS,
+  IMG_LOTTIE_HTML,
+  IMG_LOTTIE_REACT_CIRCLE,
+  IMG_LOTTIE_WEBSITE_BUILDING,
+};
