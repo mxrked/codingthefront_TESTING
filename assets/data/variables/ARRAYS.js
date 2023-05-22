20500d00220b13c19d3b1da7852384cac4cb4658
Parker <pp101socials@gmail.com>
2023-05-22 12:41:16 -0400

    Created file

--- assets/data/variables/ARRAYS.js
+++ assets/data/variables/ARRAYS.js
@@ -1,1 +1,31 @@
 // This file is used to hold some arrays
+
+const SITE_KWS = [
+  "codingthefront",
+  "coding the front",
+  "parker phelps",
+  "parker phelps web developer",
+  "html developer",
+  "react developer",
+  "nextjs developer",
+  "python developer",
+  "javascript developer",
+  "web designer",
+  "web developer",
+  "website creator",
+  "website designer",
+  "build a website",
+  "how to design a website",
+  "freelance web developer",
+  "winston-salem web developer",
+  "clemmons nc web developer",
+  "software developer",
+  "winston-salem software developer",
+  "frontend developer",
+  "front end developer",
+  "frontend engineer",
+  "front end web developer",
+  "frontend web developer",
+];
+
+export { SITE_KWS };
