34cf05a482d8b556da53ff3b1e312eb7b7c1fe88
Parker <pp101socials@gmail.com>
2023-06-02 14:37:21 -0400

    Added components

--- pages/projects.js
+++ pages/projects.js
@@ -10,10 +10,15 @@ import { useRouter } from "next/router";
 import { PageHead } from "@/assets/components/global/All/PageHead";
 
 import { TriggerExitAnimations } from "@/assets/functions/dom/triggers/TriggerExitAnimations";
+import ManipPageLink from "@/assets/functions/dom/manip/ManipPageLinks";
 
 import { DesktopNav } from "@/assets/components/global/Nav/Desktop/DesktopNav";
 import { MobileNav } from "@/assets/components/global/Nav/Mobile/MobileNav";
 import { MobileNavMenu } from "@/assets/components/global/Nav/Mobile/MobileNavMenu";
+
+import { ProjectsTop } from "@/assets/components/pages/Projects/ProjectsTop";
+import { ProjectsMain } from "@/assets/components/pages/Projects/ProjectsMain";
+
 import { Footer } from "@/assets/components/global/Footer/Footer";
 
 import styles from "../assets/styles/modules/Projects/Projects.module.css";
@@ -26,6 +31,11 @@ export default function Projects() {
     TriggerExitAnimations();
   }, []);
 
+  // Disabling Page Links
+  useEffect(() => {
+    ManipPageLink("disable", "multiple", ".projects-link");
+  }, []);
+
   return (
     <div id="PAGE" className="page overrides_Projects full-second">
       <PageHead />
@@ -35,6 +45,9 @@ export default function Projects() {
       <MobileNavMenu />
 
       <main id="PAGE_CNT">
+        <ProjectsTop />
+        <ProjectsMain />
+
         <Footer />
       </main>
     </div>
