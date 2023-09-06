546d35564754feba114b7bfe3bc5fdfaf9fa33c0
Parker <pp101socials@gmail.com>
2023-09-06 10:47:11 -0400

    Created file

--- assets/functions/dom/togglers/ToggleMobileNav.js
+++ assets/functions/dom/togglers/ToggleMobileNav.js
@@ -6,4 +6,12 @@
 
 import DeclareStorageVariable from "../../data/storage/DeclareStorageVariable";
 
-export default function ToggleMobileNav() {}
+export default function ToggleMobileNav() {
+  const DARKEN = document.getElementById("mobileNavMenuDarken");
+  const MAIN = document.getElementById("mobileNavMenuMain");
+
+  DeclareStorageVariable("session", "Mobile Nav Opened", true);
+
+  document.body.style.pointerEvents = "none";
+  document.body.style.overflowY = "hidden";
+}
