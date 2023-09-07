fe1d3d0e688a748bd2210344c6b01c3a7c283cbb
Parker <pp101socials@gmail.com>
2023-09-07 08:43:42 -0400

    Added cdn

---
+++ assets/cdns/CDNVideos.js
@@ -0,0 +1,11 @@
+/**
+ *
+ *  This holds the urls/srcs for the videos
+ *
+ */
+
+import { CDNVideoReturn } from "./CDNReturns";
+
+const INDEX_TOP_VIDEO = CDNVideoReturn("compressed", "blue-code", "mp4");
+
+export { INDEX_TOP_VIDEO };
