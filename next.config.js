2b0c59836bb09d9e8a2b055f564af028c05a1ce7
Parker <pp101socials@gmail.com>
2023-09-11 11:44:00 -0400

    Changed assetPrefix for testing and regular

--- next.config.js
+++ next.config.js
@@ -14,7 +14,20 @@ module.exports = {
 
   //! This added clean urls and eliminates the .html extension aswell as linking
   //! CSS and JS with the trailingSlash
-  assetPrefix: isProd ? "https://codingthefront.com/" : "",
+  // assetPrefix: isProd ? "https://codingthefront.com/" : "",
+  assetPrefix: (() => {
+    if (isProd) {
+      if (process.env.DOMAIN === "codingthefront.com") {
+        return "https://codingthefront.com/";
+      } else if (
+        process.env.DOMAIN === "glittering-cupcake-b97603.netlify.app"
+      ) {
+        return "https://glittering-cupcake-b97603.netlify.app/";
+      } else {
+        return "";
+      }
+    }
+  })(),
   trailingSlash: true,
 
   // This adds the _headers file after npm run build
